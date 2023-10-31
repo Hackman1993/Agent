36bb3a53444a639ae12fd600bde086fceeca28a2
Hackman.Lo <admin@yytec.net>
2023-10-31 10:35:04 +0800

    Initialize

--- cmake/template/agent.in.cmake
+++ cmake/template/agent.in.cmake
@@ -5,8 +5,9 @@ list(APPEND CMAKE_MODULE_PATH ${CMAKE_CURRENT_SOURCE_DIR})
 
 find_dependency(Boost REQUIRED COMPONENTS json)
 find_dependency(sahara REQUIRED)
+find_dependency(obelisk REQUIRED)
 
-include(${CMAKE_CURRENT_LIST_DIR}/obelisk-targets.cmake)
+include(${CMAKE_CURRENT_LIST_DIR}/agent-targets.cmake)
 if(@Boost_FOUND@)
   include_directories(${Boost_INCLUDE_DIRS})
 endif()
